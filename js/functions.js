c3854d4c9b10ba80d251e69b33754e3465fc2d99
Laurent BOURGEOIS <laurent_bourgeois@yahoo.fr>
2016-09-16 01:31:22 +0200

    Proto

--- js/functions.js
+++ js/functions.js
@@ -1,7 +1,34 @@
-function filtrage(catalogue) {
+/**
+* Applique un filtre sur le catalogue
+* Retourne le tableau contenant les éléments filtrés
+**/
+function filtrage(catalogue, filtres) {
 
   return $.grep( catalogue, function( n, i ) {
-    return n.website==='google';
+    var retour = true;
+    // Boucle sur les filtres
+    for (var i = 0; i < filtres.length; i++) {
+      if (filtres[i] != null) {
+        retour = filtres[i] === n[i];
+        if (!retour) break;
+      }
+    }
+    return retour;
   });
 
+}
+
+function toAssociativeArray(catalogue){
+  var retour = new Object();
+  // Boucle sur les enregistrements
+  for (var i = 0; i < catalogue.length; i++) {
+    var row = catalogue[i];
+    // Si l'arbo jusqu'à la feuille n'existe pas on la créé
+    if (typeof retour[row[0]] === 'undefined'){
+      retour[row[0]] = []
+    }
+    // Ajout de la valeur de la feuille
+    retour[row[0]].push(row[1]);
+  }
+  return retour;
 }
