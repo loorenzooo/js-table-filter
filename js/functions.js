1d430310ec43dd8b74f2cdd884acf6f71e8070c6
Laurent BOURGEOIS <laurent.bourgeois_ssii@i-bp.fr>
2016-09-16 18:27:47 +0200

    dev en cours

--- js/functions.js
+++ js/functions.js
@@ -26,6 +26,7 @@ function toAssociativeArray(catalogue){
     // Si l'arbo jusqu'à la feuille n'existe pas on la créé
     if (typeof retour[row[0]] === 'undefined'){
       retour[row[0]] = new Object();
+      retour[row[0]].label = row[0];
       retour[row[0]].items = [];
       retour[row[0]].rowspan = 0
     }
@@ -37,11 +38,26 @@ function toAssociativeArray(catalogue){
 }
 
 function toHtml(catalogue){
+
   var retour = "<table>";
-  for (item in catalogue){
-    retour += "<tr>";
+  for (var niv1_prop in catalogue){
+    // Niveau 1
+    var niv1 = catalogue[niv1_prop];
+    retour += "<tr rowspan='"+niv1.rowspan+"'>";
+    retour += "<td>"+niv1.label+"</td>";
     retour += "</tr>";
+
+    // Niveau 1 container
+    retour += "<div class='left'>";
+    // Niveau 2
+    for (var niv2_prop in niv1.items){
+      var niv2 = niv1.items[niv2_prop];
+      retour += "<div>";
+      retour += niv2;
+      retour += "</div>";
+    }
+    retour += "</div>";
   }
-  retour += "</table>";
+  retour += "</div>";
   return retour;
 }
