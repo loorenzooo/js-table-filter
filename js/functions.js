b22d9dd0f272921e7e969f1c395dda327a212958
Laurent BOURGEOIS <laurent_bourgeois@yahoo.fr>
2016-09-16 02:07:37 +0200

    Dev en cours

--- js/functions.js
+++ js/functions.js
@@ -25,10 +25,23 @@ function toAssociativeArray(catalogue){
     var row = catalogue[i];
     // Si l'arbo jusqu'à la feuille n'existe pas on la créé
     if (typeof retour[row[0]] === 'undefined'){
-      retour[row[0]] = []
+      retour[row[0]] = new Object();
+      retour[row[0]].items = [];
+      retour[row[0]].rowspan = 0
     }
     // Ajout de la valeur de la feuille
-    retour[row[0]].push(row[1]);
+    retour[row[0]].items.push(row[1]);
+    retour[row[0]].rowspan = retour[row[0]].rowspan + 1;
   }
+  return retour;
+}
+
+function toHtml(catalogue){
+  var retour = "<table>";
+  for (item in catalogue){
+    retour += "<tr>";
+    retour += "</tr>";
+  }
+  retour += "</table>";
   return retour;
 }
